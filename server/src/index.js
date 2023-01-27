6c19ca08156eb67295dedd9ad743a1d0e5faac7d
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-01-27 23:45:53 +0600

    OpenAI 's API Connected

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const OPENAI_API_KEY = "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";
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+openai.listEngines().then((response) => {
+  console.log(response);
+});
+
+openai
+  .createCompletion({
+    model: "text-davinci-003",
+    prompt: "What is APi",
+    max_tokens: 100,
+    temperature: 0,
+  })
+  .then((response) => {
+    console.log(response.data);
+  });
 
 const app = express();
+
 app.use(express.json());
 
 app.get("/ping", (req, res) => {
